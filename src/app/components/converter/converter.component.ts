0f2e74008a8aeb1c7eb61a32e3b909dae3c89f9a
AvigailShlomov <anm798@gmail.com>
2024-12-22 14:05:21 +0200

    also change to generic and sparate part of initChartOptions method to a help method instead

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,6 +10,7 @@ import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 import { ConverterService } from '../../services/converter.service';
 import { HistoryService } from '../../services/history.service';
 import { convertStringToDate } from '../../utils/utils';
+import { Currencies, CURRENCIES_STORAGE_KEY, HISTORY_STORAGE_KEY } from '../../models/currency.models';
 
 @Component({
   selector: 'app-converter',
@@ -40,7 +40,7 @@ export class ConverterComponent implements OnInit {
   public resultToCuur: string = 'ILS';
   public resultFromCuur: string = 'USD';
   public resultAmount: number = 1;
-
+  
   constructor(
     private converterService: ConverterService,
     private historyService: HistoryService
@@ -51,16 +51,29 @@ export class ConverterComponent implements OnInit {
     this.getAllCurrencies();
   }
 
-  getAllCurrencies() {
-    this.converterService.getAllCurrencies().subscribe({
-      next: (currencies) => {
-        this.currDisplay = Object.entries(currencies).map(([key, value]) => ({
+  private getAllCurrencies() {
+    const currenciesFromStorage = this.historyService.getDataFromStorage<Currencies>(CURRENCIES_STORAGE_KEY);
+    if (currenciesFromStorage.length > 0) {
+
+      this.currDisplay = currenciesFromStorage.flatMap(currencyObject =>
+        Object.entries(currencyObject).map(([key, value]) => ({
           key,
-          value,
-        }));
-      },
-      error: (err) => alert('Error while fetching currency List'),
-    });
+          value
+        }))
+      );
+    }
+    else {// if the currencies doesn't exist in LS
+      this.converterService.getAllCurrencies().subscribe({
+        next: (currencies) => {
+          this.currDisplay = Object.entries(currencies).map(([key, value]) => ({
+            key,
+            value,
+          }));
+          this.historyService.saveDataToStorage(CURRENCIES_STORAGE_KEY, currencies);
+        },
+        error: (err) => alert('Error while fetching currency List'),
+      });
+    }
   }
 
   convert() {
@@ -74,13 +87,14 @@ export class ConverterComponent implements OnInit {
       .subscribe({
         next: (res) => {
           this.result = res.rates[this.toCurrency];
-          this.historyService.saveConvertionToStorage({
-            amount: this.amount,
-            from: this.fromCurrency,
-            to: this.toCurrency,
-            result: this.result,
-            date: new Date(),
-          });
+          this.historyService.saveDataToStorage(HISTORY_STORAGE_KEY,
+            {
+              amount: this.amount,
+              from: this.fromCurrency,
+              to: this.toCurrency,
+              result: this.result,
+              date: new Date(),
+            });
           this.resultToCuur = this.toCurrency;
           this.resultFromCuur = this.fromCurrency;
           this.resultAmount = this.amount;
@@ -90,11 +104,10 @@ export class ConverterComponent implements OnInit {
       });
   }
 
-  updateChart() {
+  private updateChart() {
     let chartXandYValues: { x: Date; y: number }[] = [];
 
-    this.converterService
-      .getHistoricalRates(this.fromCurrency, this.toCurrency)
+    this.converterService.getHistoricalRates(this.fromCurrency, this.toCurrency)
       .subscribe({
         next: (data) => {
           const dates = Object.keys(data.rates);
@@ -103,44 +116,48 @@ export class ConverterComponent implements OnInit {
             y: data.rates[date][this.toCurrency],
           }));
 
-          if (!this.chartOptions) {
-            this.chartOptions = {
-              theme: 'light2',
-              animationEnabled: true,
-              zoomEnabled: true,
-              title: {
-                text: `Exchange Rate`,
-              },
-              axisY: {
-                labelFormatter: (e: any) => {
-                  const suffixes = ['', 'K', 'M', 'B', 'T'];
-                  let order = Math.max(
-                    Math.floor(Math.log(e.value) / Math.log(1000)),
-                    0
-                  );
-                  order = Math.min(order, suffixes.length - 1);
-                  const suffix = suffixes[order];
-                  return e.value / Math.pow(1000, order) + suffix;
-                },
-              },
-              data: [
-                {
-                  type: 'line',
-                  xValueFormatString: 'YYYY-MM-DD',
-                  yValueFormatString: '#,###.##',
-                  dataPoints: [...chartXandYValues],
-                },
-              ],
-            };
-          } else {
-            this.chartOptions.data[0].dataPoints = [...chartXandYValues];
-          }
-
+          this.initChartOptions(chartXandYValues);
           if (this.chart) {
             this.chart.render();
           }
         },
         error: (err) => console.error('Error fetching historical rates:', err),
       });
+  }
+
+  private initChartOptions(chartXandYValues: { x: Date; y: number }[]) {
+    if (!this.chartOptions) {
+      this.chartOptions = {
+        theme: 'light2',
+        animationEnabled: true,
+        zoomEnabled: true,
+        title: {
+          text: `Exchange Rate`,
+        },
+        axisY: {
+          labelFormatter: (e: any) => {
+            const suffixes = ['', 'K', 'M', 'B', 'T'];
+            let order = Math.max(
+              Math.floor(Math.log(e.value) / Math.log(1000)),
+              0
+            );
+            order = Math.min(order, suffixes.length - 1);
+            const suffix = suffixes[order];
+            return e.value / Math.pow(1000, order) + suffix;
+          },
+        },
+        data: [
+          {
+            type: 'line',
+            xValueFormatString: 'YYYY-MM-DD',
+            yValueFormatString: '#,###.##',
+            dataPoints: [...chartXandYValues],
+          },
+        ],
+      };
+    } else {
+      this.chartOptions.data[0].dataPoints = [...chartXandYValues];
+    }
+
   }
 }
