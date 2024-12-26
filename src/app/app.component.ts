ca6830a2a70688a9b1963fdf51d50dcc53ee5872
AvigailShlomov <anm798@gmail.com>
2024-12-26 10:40:17 +0200

    redundent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConverterComponent } from './components/converter/converter.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HistoryComponent } from './components/History/history.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent,
-    ConverterComponent,
-    HistoryComponent
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'currencyConverter';
 }
